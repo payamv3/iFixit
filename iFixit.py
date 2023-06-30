5ce61d356c2807f4053cc4be12131e4562ac69bd
Payam Saeedi <76733438+payamv3@users.noreply.github.com>
2023-06-30 16:52:24 -0400

    Add files via upload

--- iFixit.py
+++ iFixit.py
@@ -6,9 +6,11 @@ import altair as alt
 from bs4 import BeautifulSoup as bs
 import requests
 
-st.sidebar.header("How has repairability of Device Name changed over time")
+st.sidebar.header("How has the repairability of Laptops, Tablets and Smartphones changed over time?")
 st.sidebar.markdown("""Source: iFixit Repairability Scores""")
-st.sidebar.markdown("""A small project by Payam Saeedi""")
+st.sidebar.markdown("""A small data-driven story by Payam Saeedi""")
+
+tab1, tab2, tab3 = st.tabs(["Smartphones", "Tablets", "Laptops"])
 
 def scrape_iFixit(url):
     r = r=requests.get(url)
@@ -72,114 +74,122 @@ for index, row in iFixit_Laptop.iterrows():
 iFixit_Laptop['Score'] = iFixit_Laptop['Score'].astype('int8')
 
 #Workflow begins here
+#Smartphone data
+with tab1:
 
-brand_select = st.multiselect("Type the names of the brands you would like to compare, or select from the drop-down menu", pd.unique(iFixit['Brand']))
+    brand_select = st.multiselect("Type the names of the brands you would like to compare, or select from the drop-down menu", pd.unique(iFixit['Brand']))
 
-iFixit1 = iFixit[iFixit['Brand'].isin(brand_select)]
-data = iFixit1.groupby(['Year','Brand'], as_index = False).Score.agg('mean')
-data['Score'] = data['Score'].round(decimals = 2)
+    iFixit1 = iFixit[iFixit['Brand'].isin(brand_select)]
+    data = iFixit1.groupby(['Year','Brand'], as_index = False).Score.agg('mean')
+    data['Score'] = data['Score'].round(decimals = 2)
 
-def get_dataset():
-    return data.pivot(index = 'Year', columns = 'Brand', values = 'Score')
+    def get_dataset():
+        return data.pivot(index = 'Year', columns = 'Brand', values = 'Score')
 
-df = get_dataset()
+    df = get_dataset()
 
-Lines_Phones = alt.Chart(data).mark_line().encode(
-    alt.X('Year:O', axis = alt.Axis(title = 'Year',labels = True, grid = False, labelAngle=0, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
-    alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray')),
-    # The highlight will be set on the result of a conditional statement
-    color=alt.Color('Brand:N', scale=alt.Scale(), legend = alt.Legend(title = None)),
-    tooltip=['Year','Score']
+    Lines_Phones = alt.Chart(data).mark_line().encode(
+        alt.X('Year:O', axis = alt.Axis(title = 'Year',labels = True, grid = False, labelAngle=0, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
+        alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray')),
+        # The highlight will be set on the result of a conditional statement
+        color=alt.Color('Brand:N', scale=alt.Scale(), legend = alt.Legend(title = None)),
+        tooltip=['Year','Score']
 
-).properties(title ="Average Repairability Score of Different Brands through Time", width = 1000, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
+    ).properties(title ="Average Repairability Score of Different Brands through Time", width = 1000, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
 
-st.altair_chart(Lines_Phones)
+    st.altair_chart(Lines_Phones)
 
-brand_filter = st.selectbox("Select a brand to display the repairability score for each model", pd.unique(iFixit['Brand']))
+    brand_filter = st.selectbox("Select a brand to display the repairability score for each model", pd.unique(iFixit['Brand']))
 
-iFixit = iFixit[iFixit['Brand'] == (brand_filter)]
-iFixit = iFixit.sort_values(by = ['Year'], ascending = False)
+    iFixit = iFixit[iFixit['Brand'] == (brand_filter)]
+    iFixit = iFixit.sort_values(by = ['Year'], ascending = False)
 
-#st.bar_chart(iFixit, x ='Device Name', y='Score')
+    #st.bar_chart(iFixit, x ='Device Name', y='Score')
 
-Bars_Phones = alt.Chart(iFixit).mark_bar().encode(
-    alt.X('Device Name:N', sort = None, axis = alt.Axis(title = 'Brand',labels = True, grid = False, labelAngle=-90, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
-    alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray'))
+    Bars_Phones = alt.Chart(iFixit).mark_bar().encode(
+        alt.X('Device Name:N', sort = None, axis = alt.Axis(title = 'Brand',labels = True, grid = False, labelAngle=-90, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
+        alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray'))
 
-).properties(title ="Repairability Scores per Brand", width = 800, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
+    ).properties(title ="Repairability Scores per Brand", width = 800, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
 
-st.altair_chart(Bars_Phones)
+    st.altair_chart(Bars_Phones)
 
-brand_select = st.multiselect("Type the names of the brands you would like to compare, or select from the drop-down menu", pd.unique(iFixit_Tablet['Brand']))
+#Tablet data:
+with tab2:
 
-iFixit1_Tablet = iFixit_Tablet[iFixit_Tablet['Brand'].isin(brand_select)]
-data_Tablet = iFixit1_Tablet.groupby(['Year','Brand'], as_index = False).Score.agg('mean')
-data_Tablet['Score'] = data_Tablet['Score'].round(decimals = 2)
+    brand_select = st.multiselect("Type the names of the brands you would like to compare, or select from the drop-down menu", pd.unique(iFixit_Tablet['Brand']))
 
-def get_dataset_Tablet():
-    return data_Tablet.pivot(index = 'Year', columns = 'Brand', values = 'Score')
+    iFixit1_Tablet = iFixit_Tablet[iFixit_Tablet['Brand'].isin(brand_select)]
+    data_Tablet = iFixit1_Tablet.groupby(['Year','Brand'], as_index = False).Score.agg('mean')
+    data_Tablet['Score'] = data_Tablet['Score'].round(decimals = 2)
 
-df_Tablet = get_dataset_Tablet()
+    def get_dataset_Tablet():
+        return data_Tablet.pivot(index = 'Year', columns = 'Brand', values = 'Score')
 
-Lines_Tablet = alt.Chart(data_Tablet).mark_line().encode(
-    alt.X('Year:O', axis = alt.Axis(title = 'Year',labels = True, grid = False, labelAngle=0, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
-    alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray')),
-    # The highlight will be set on the result of a conditional statement
-    color=alt.Color('Brand:N', scale=alt.Scale(), legend = alt.Legend(title = None)),
-    tooltip=['Year','Score']
+    df_Tablet = get_dataset_Tablet()
 
-).properties(title ="Average Repairability Score of Different Brands through Time", width = 1000, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
+    Lines_Tablet = alt.Chart(data_Tablet).mark_line().encode(
+        alt.X('Year:O', axis = alt.Axis(title = 'Year',labels = True, grid = False, labelAngle=0, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
+        alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray')),
+        # The highlight will be set on the result of a conditional statement
+        color=alt.Color('Brand:N', scale=alt.Scale(), legend = alt.Legend(title = None)),
+        tooltip=['Year','Score']
 
-st.altair_chart(Lines_Tablet)
+    ).properties(title ="Average Repairability Score of Different Brands through Time", width = 1000, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
 
-brand_filter = st.selectbox("Select a brand to display the repairability score for each model", pd.unique(iFixit_Tablet['Brand']))
+    st.altair_chart(Lines_Tablet)
 
-iFixit_Tablet = iFixit_Tablet[iFixit_Tablet['Brand'] == (brand_filter)]
-iFixit_Tablet = iFixit_Tablet.sort_values(by = ['Year'], ascending = False)
+    brand_filter = st.selectbox("Select a brand to display the repairability score for each model", pd.unique(iFixit_Tablet['Brand']))
 
-#st.bar_chart(iFixit, x ='Device Name', y='Score')
+    iFixit_Tablet = iFixit_Tablet[iFixit_Tablet['Brand'] == (brand_filter)]
+    iFixit_Tablet = iFixit_Tablet.sort_values(by = ['Year'], ascending = False)
 
-Bars_Tablet = alt.Chart(iFixit_Tablet).mark_bar().encode(
-    alt.X('Device Name:N', sort = None, axis = alt.Axis(title = 'Brand',labels = True, grid = False, labelAngle=-90, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
-    alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray'))
+    #st.bar_chart(iFixit, x ='Device Name', y='Score')
 
-).properties(title ="Repairability Scores per Brand", width = 800, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
+    Bars_Tablet = alt.Chart(iFixit_Tablet).mark_bar().encode(
+        alt.X('Device Name:N', sort = None, axis = alt.Axis(title = 'Brand',labels = True, grid = False, labelAngle=-90, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
+        alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray'))
 
-st.altair_chart(Bars_Tablet)
+    ).properties(title ="Repairability Scores per Brand", width = 800, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
 
-brand_select = st.multiselect("Type the names of the brands you would like to compare, or select from the drop-down menu", pd.unique(iFixit_Laptop['Brand']))
+    st.altair_chart(Bars_Tablet)
 
-iFixit1_Laptop = iFixit_Laptop[iFixit_Laptop['Brand'].isin(brand_select)]
-data_Laptop = iFixit1_Laptop.groupby(['Year','Brand'], as_index = False).Score.agg('mean')
-data_Laptop['Score'] = data_Laptop['Score'].round(decimals = 2)
+#Laptop data:
+with tab3:
+    
+    brand_select = st.multiselect("Type the names of the brands you would like to compare, or select from the drop-down menu", pd.unique(iFixit_Laptop['Brand']))
 
-def get_dataset_Laptop():
-    return data_Laptop.pivot(index = 'Year', columns = 'Brand', values = 'Score')
+    iFixit1_Laptop = iFixit_Laptop[iFixit_Laptop['Brand'].isin(brand_select)]
+    data_Laptop = iFixit1_Laptop.groupby(['Year','Brand'], as_index = False).Score.agg('mean')
+    data_Laptop['Score'] = data_Laptop['Score'].round(decimals = 2)
 
-df_Laptop = get_dataset_Laptop()
+    def get_dataset_Laptop():
+        return data_Laptop.pivot(index = 'Year', columns = 'Brand', values = 'Score')
 
-Lines_Laptop = alt.Chart(data_Laptop).mark_line().encode(
-    alt.X('Year:O', axis = alt.Axis(title = 'Year',labels = True, grid = False, labelAngle=0, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
-    alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray')),
-    # The highlight will be set on the result of a conditional statement
-    color=alt.Color('Brand:N', scale=alt.Scale(), legend = alt.Legend(title = None)),
-    tooltip=['Year','Score']
+    df_Laptop = get_dataset_Laptop()
 
-).properties(title ="Average Repairability Score of Different Brands through Time", width = 1000, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
+    Lines_Laptop = alt.Chart(data_Laptop).mark_line().encode(
+        alt.X('Year:O', axis = alt.Axis(title = 'Year',labels = True, grid = False, labelAngle=0, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
+        alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray')),
+        # The highlight will be set on the result of a conditional statement
+        color=alt.Color('Brand:N', scale=alt.Scale(), legend = alt.Legend(title = None)),
+        tooltip=['Year','Score']
 
-st.altair_chart(Lines_Laptop)
+    ).properties(title ="Average Repairability Score of Different Brands through Time", width = 1000, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
 
-brand_filter = st.selectbox("Select a brand to display the repairability score for each model", pd.unique(iFixit_Laptop['Brand']))
+    st.altair_chart(Lines_Laptop)
 
-iFixit_Laptop = iFixit_Laptop[iFixit_Laptop['Brand'] == (brand_filter)]
-iFixit_Laptop = iFixit_Laptop.sort_values(by = ['Year'], ascending = False)
+    brand_filter = st.selectbox("Select a brand to display the repairability score for each model", pd.unique(iFixit_Laptop['Brand']))
 
-#st.bar_chart(iFixit, x ='Device Name', y='Score')
+    iFixit_Laptop = iFixit_Laptop[iFixit_Laptop['Brand'] == (brand_filter)]
+    iFixit_Laptop = iFixit_Laptop.sort_values(by = ['Year'], ascending = False)
 
-Bars_Laptop = alt.Chart(iFixit_Laptop).mark_bar().encode(
-    alt.X('Device Name:N', sort = None, axis = alt.Axis(title = 'Brand',labels = True, grid = False, labelAngle=-90, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
-    alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray'))
+    #st.bar_chart(iFixit, x ='Device Name', y='Score')
 
-).properties(title ="Repairability Scores per Brand", width = 800, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
+    Bars_Laptop = alt.Chart(iFixit_Laptop).mark_bar().encode(
+        alt.X('Device Name:N', sort = None, axis = alt.Axis(title = 'Brand',labels = True, grid = False, labelAngle=-90, labelFontSize=14, tickSize=0, labelPadding=10, labelColor = 'gray')),
+        alt.Y('Score:Q', axis= alt.Axis(grid = True, labelAngle= 0, labelFontSize = 14, tickSize = 0, labelColor = 'gray'))
 
-st.altair_chart(Bars_Laptop)
+    ).properties(title ="Repairability Scores per Brand", width = 800, height = 400).configure_view(stroke = 'transparent',strokeWidth=0, strokeOpacity = 0).configure_title(anchor='start', color = 'grey').configure_axisX(titleAlign='left')
+
+    st.altair_chart(Bars_Laptop)
